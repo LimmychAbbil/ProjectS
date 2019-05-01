36f70c0b00852ff57669ae15b59d7be2e418442b
Limmy <limych24@gmail.com>
2019-05-01 17:06:32 +0300

    0.01a: Initial commit from springInitializer

---
+++ src/main/java/net/lim/projectS/ProjectSApplication.java
@@ -0,0 +1,13 @@
+package net.lim.projectS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSApplication.class, args);
+	}
+
+}
