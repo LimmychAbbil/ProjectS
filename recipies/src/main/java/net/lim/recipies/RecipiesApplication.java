783de61530d41597aaa3725d73ff77d8e4226ca5
Limmy <limych24@gmail.com>
2019-05-29 21:31:57 +0300

    Recipies: Add stub for assignment project

---
+++ recipies/src/main/java/net/lim/recipies/RecipiesApplication.java
@@ -0,0 +1,13 @@
+package net.lim.recipies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipiesApplication.class, args);
+    }
+
+}
