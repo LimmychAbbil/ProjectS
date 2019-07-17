e60008a2d4fe637b67b976c5527e79658c8d0192
Limmy <limych24@gmail.com>
2019-07-17 09:17:58 +0300

    0.01f: Add plus icon

---
+++ recipes/src/main/java/net/lim/recipes/config/WebMVCConfig.java
@@ -0,0 +1,15 @@
+package net.lim.recipes.config;
+
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+}
